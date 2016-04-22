40df72a02d1f264043b6df9fc5b4479893ba5a02
Colin Gross <grosscol@gmail.com>
2016-04-22 00:56:01 -0400

    refactor to have some class.

--- lib/auto_togglr.rb
+++ lib/auto_togglr.rb
@@ -1,47 +1,62 @@
 require 'togglv8'
 require 'date'
-require 'yaml'
 require 'pry'
 
+class AutoTogglr
 
-# Read the config
-cfg = YAML.load_file('./config/auto_toggl.yml')
-api_token = cfg["api_token"]
-default_workspace_id = cfg["default_workspace_id"]
-proj_id=cfg["default_project_id"]
+  # API attributes
+  attr :toggl_api, :user, :workspace 
 
-# Have token or die
-abort("no api_token") unless api_token
+  # Entry attributes
+  attr :description, :project_id, :start, :duration, :tag, :now
 
-# Configure the api interface
-toggl_api    = TogglV8::API.new(api_token)
-user         = toggl_api.me(all=true)
-workspaces   = toggl_api.my_workspaces(user)
-ws_id        = default_workspace_id || workspaces.first['id']
+  # initialize with a profile
+  def initialize(pf)
+    puts "Initialize for #{pf['uniqname']}"
+    # Have token or die
+    abort("no api_token") unless pf['api_token']
 
-now = DateTime.now
+    self.now = DateTime.now
+    self.toggle_api  = TogglV8::API.new(pf['api_token'])
+    self.user        = toggl_api.me(all=true)
+    self.workspace   = pf['workspace_id']
+    self.project     = pf['project_id']
+    self.description = pf['description']
+    self.start_time  = DateTime.new(now.year, now.month, now.day,
+                               pf['start']['hour'],
+                               pf['start']['minute'],
+                               pf['start']['second'],
+                               "-0400")
+    self.duration = pf['duration']
+    self.tag = pf['tag']
+  end
 
-# Check to see if there is an entry for today.
-start_date = DateTime.new(now.year, now.month, now.day, 0)
-end_date = DateTime.new(now.year, now.month, now.day, 23)
-today_entries = toggl_api.get_time_entries({
-  start_date: start_date.iso8601,
-  end_date: end_date.iso8601
-})
+  def information
+    workspaces = toggl_api.my_workspaces(user)
+  end
 
-binding.pry
-abort("today already has an entry") if today_entries.size > 0
+  def today_has_entry?
+    start_date = DateTime.new(now.year, now.month, now.day, 0)
+    end_date   = DateTime.new(now.year, now.month, now.day, 23)
+    today_entries = toggl_api.get_time_entries({
+      start_date: start_date.iso8601,
+      end_date: end_date.iso8601
+    })
 
-# Create a time entry for today
-start_time = DateTime.new(now.year, now.month, now.day, 8, 03, 00, "-0400")
+    today_entries.size > 0
+  end
 
-toggl_api.create_time_entry({
-  'description' => "Hydra Work",
-  'wid' => ws_id,
-  'pid' => proj_id,
-  'duration' => 26640,
-  'start' => start_time.iso8601,
-  'tags' => ['development'],
-  'created_with' => "Otto T. Oggle"
-})
+  def create_todays_entry
+    start_time = DateTime.new(now.year, now.month, now.day, 8, 03, 00, "-0400")
 
+    toggl_api.create_time_entry({
+      'description' => "Hydra Work",
+      'wid' => ws_id,
+      'pid' => proj_id,
+      'duration' => 26640,
+      'start' => start_time.iso8601,
+      'tags' => ['development'],
+      'created_with' => "Otto T. Oggle"
+    })
+  end
+end
