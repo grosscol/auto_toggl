5c8e5e0f4a6a3c94c543c64a02617e8b7ee22e85
Colin Gross <grosscol@gmail.com>
2016-04-22 00:54:50 -0400

    Add class to hold global settings.

---
+++ lib/settings.rb
@@ -0,0 +1,20 @@
+require 'yaml'
+
+class Settings
+  @@profiles 
+
+  def self.profiles
+    @@profiles ||= parse_config_file
+  end
+
+  private
+
+  def self.parse_config_file
+    path = File.expand_path('../../config/auto_toggl.yml', __FILE__) 
+    cfg = YAML.load_file(path)
+    cfg['profiles']
+  end
+end
+
+puts 'start'
+puts Settings.profiles
